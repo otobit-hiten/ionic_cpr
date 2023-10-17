d71c1f669f0ce7f7ecdf0338900d82a892b8642f
otobit-hiten <hiten@otobit.in>
2023-10-17 15:46:06 +0530

    map

--- src/app/location/location.page.ts
+++ src/app/location/location.page.ts
@@ -1,8 +1,12 @@
-import { CUSTOM_ELEMENTS_SCHEMA, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, ChangeDetectorRef, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { GoogleMap } from '@capacitor/google-maps';
+import { RouterModule } from '@angular/router';
+import { Geolocation } from '@capacitor/geolocation';
+import { LatLng, Marker } from '@capacitor/google-maps/dist/typings/definitions';
+import { animate } from '@angular/animations';
 
 @Component({
 
@@ -12,37 +16,88 @@ import { GoogleMap } from '@capacitor/google-maps';
   standalone: true,
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 
-  imports: [IonicModule, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule,RouterModule]
 })
 export class LocationPage implements OnInit {
-  @ViewChild('map')mapRef!:ElementRef;
+  @ViewChild('map')mapRef:ElementRef | undefined;
   newMap!: GoogleMap;
+  marker :any;
+  cordinates: LatLng = {lat: 0, lng: 0}
+  drag:boolean = false;
 
 
+  constructor(private changeRef: ChangeDetectorRef) {
+    
+  }
+
+   async ngOnInit() {
+    let perm = await Geolocation.requestPermissions();
+   }
 
   ionViewDidEnter(){
+    this.currentPosition()
     this.createMap()
   }
+   currentPosition = async () => {
+    const coordinates = await Geolocation.getCurrentPosition();
+    console.log('Current position:', coordinates);
+    this.cordinates = {lat : coordinates.coords.latitude, lng : coordinates.coords.longitude}
+    console.log('Current position:', this.cordinates);
+  };
 
   async createMap() {
     this.newMap = await GoogleMap.create({
       id: 'my-cool-map',
-      element: this.mapRef.nativeElement,
+      element: this.mapRef?.nativeElement,
       apiKey: 'AIzaSyCz_7nUaWof9wvkUG-1x7SnecvG5eLx95Y',
       config: {
-        center: {
-          lat: 33.6,
-          lng: -117.9,
-        },
-        zoom: 8,
+        center: this.cordinates,
+        zoom: 15,
       },
+
     });
-  }
-  constructor() {
+
+    const markerId = await this.newMap.addMarker({
+      coordinate: this.cordinates,
+      draggable : true,
+      
+    });
+    
+    await this.newMap.setCamera({
+      coordinate: this.cordinates,
+      animate:true
+    });
+
+
+    
+     await this.newMap.setOnMarkerDragListener((event) => {
+      this.drag = true;
+         this.cordinates = {lat: event.latitude, lng: event.longitude}
+        console.log('click',this.cordinates);
+        this.changeRef.detectChanges();
+
+    });
+
+    await this.newMap.setOnMapClickListener((event) => {
+      // console.log('click',event);
+      // this.cordinates = {lat : event.latitude, lng : event.longitude};
+      // this.newMap.addMarker({
+      //   coordinate: this.cordinates,
+      //   draggable : true,
+      // });
+    })
+    
+
+
   }
 
-   ngOnInit() {
+
+
+   async ngOnDestroy(){
+    await this.newMap.destroy();
    }
 
   
 }
+
+
